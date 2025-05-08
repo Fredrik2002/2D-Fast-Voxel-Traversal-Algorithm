9bc5d2c738a6632057cedc9aa1876d80de194efe
Fredrik2002 <fredrik04122002@gmail.com>
2025-05-08 18:17:06 +0200

    bugfix

--- FastVoxelTransversal.py
+++ FastVoxelTransversal.py
@@ -1,6 +1,33 @@
 from math import floor, sqrt, inf, ceil
 
 
+def get_start_voxel(start, step_vector):
+    """
+    If the starting point is on the edge of a voxel, we 'choose' the closest voxel towards the end point
+    """
+    start_voxel = []
+    for i in range(len(start)):
+        x = start[i]
+        if step_vector[i] < 0 and floor(x) == x:
+            start_voxel.append(floor(x)-1)
+        else:
+            start_voxel.append(floor(x))
+    return start_voxel
+
+def get_end_voxel(end, step_vector):
+    """
+    If the end point is on the edge of a voxel, we 'choose' the closest voxel towards the start point
+    """
+    end_voxel = []
+    for i in range(len(end)):
+        x = end[i]
+        if step_vector[i] > 0 and floor(x) == x:
+            end_voxel.append(floor(x)-1)
+        else:
+            end_voxel.append(floor(x))
+    return end_voxel
+
+
 def FastVoxelTraversal(start, end):
     """
     Parameters :
@@ -8,6 +35,8 @@ def FastVoxelTraversal(start, end):
     - start : tuple of size n representing the starting point of our segment
     - end : tuple of size n representing the endpoint of our segment
     """
+    if tuple(start) == tuple(end):
+        return [floor(x) for x in start]
     vector = []
     step_vector = []
     for start_i, end_i in zip(start, end):
@@ -18,26 +47,24 @@ def FastVoxelTraversal(start, end):
             step_vector.append(0)
         else:
             step_vector.append(-1)
-    vector_norm = sqrt(sum(e**2 for e in vector))
-    vector = [abs(e/vector_norm) for e in vector]
 
-    current_point = [floor(element) for element in start]
-    end_point = [floor(element) for element in end]
+    current_point = [floor(element) for element in start] #get_start_voxel(start, step_vector)
+    end_point = get_end_voxel(end, step_vector)
 
     L_points = [tuple(current_point)]
-    tDelta = [1/vector[i] if vector[i]!=0 else inf for i in range(len(start))]
+    tDelta = [1/abs(vector[i]) if vector[i]!=0 else inf for i in range(len(start))]
     tMax = []
     for i in range(len(start)):
         x = start[i]
         if vector[i] == 0:
             distance = inf
+            tMax.append(distance)
         elif step_vector[i] == 1:
-            distance = ceil(x)-x
+            distance = floor(x)+1-x
+            tMax.append(distance/abs(vector[i]))
         else:
             distance = x-floor(x)
-        if distance == 0:
-            distance = 1
-        tMax.append(distance/vector[i])
+            tMax.append(distance/abs(vector[i]))
     while current_point != end_point:
         direction = min(range(len(tMax)), key=tMax.__getitem__)
         tMax[direction] += tDelta[direction]
@@ -46,4 +73,5 @@ def FastVoxelTraversal(start, end):
     return L_points
 
 
-print(FastVoxelTraversal((7,1), (8,0)))
+if __name__ == "__main__":
+    print(FastVoxelTraversal((5,3), (3, 5)))
